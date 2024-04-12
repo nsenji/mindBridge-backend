0437734c0caf0dce27867e75308a0acca84bb221
victor-nsengiyumva <jerrysyre@gmail.com>
2024-04-12 19:51:18 +0300

    created the add diagnosis endpoint

---
+++ controllers/get_patient_diagnoses.js
@@ -0,0 +1,18 @@
+const database = require("../database/index")
+
+const Diagnosis = database.Diagnosis
+
+
+exports.getPatientDiagnoses = async (req, res, next) => {
+    const incomingPatientID = req.body.patientID
+    try {
+        var response = await Diagnosis.findAll({ where: { patientID: incomingPatientID } });
+        return res.status(201).json({ data: response })
+
+    } catch (error) {
+        res.status(400).json({ message: "Couldn't get diagnoses" + error })
+
+    }
+
+
+}
--- routes/diagnosis.js
+++ routes/diagnosis.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 
 const addDiagnosisController = require("../controllers/add_diagnosis");
+const getPatientDiagnoses = require("../controllers/get_patient_diagnoses");
 
 router.post("/postdiagnosis", addDiagnosisController.add_diagnosis);
+router.post("/getdiagnoses", getPatientDiagnoses.getPatientDiagnoses);
 
 
 module.exports = router;
