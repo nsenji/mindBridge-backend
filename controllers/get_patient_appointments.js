0c62be08ae905842ba52cafc0e3d6b09bdc8de47
victor-nsengiyumva <jerrysyre@gmail.com>
2024-04-15 01:27:01 +0300

    done with getting all the patients appointments

---
+++ controllers/get_patient_appointments.js
@@ -0,0 +1,32 @@
+const database = require("../database/index")
+const moment = require('moment');
+
+
+const SelectedAppointment = database.SelectedAppointment
+
+
+exports.getPatientAppointments = async (req, res, next) => {
+    const incomingPatientID = req.body.patientID
+    try {
+        var response = await SelectedAppointment.findAll({
+            where: {
+                patientID: incomingPatientID, status: "active",
+            }
+        });
+
+        activeAppointments = response.filter((value) => {
+            const dateString = value.date;
+            const formattedDate = moment(dateString, "ddd, DD MMM YYYY").format("YYYY-MM-DD");
+            const currentDate = moment();
+            return formattedDate >= currentDate.format("YYYY-MM-DD")
+        })
+        
+        return res.status(201).json({ data: activeAppointments })
+
+    } catch (error) {
+        return res.status(400).json({ message: "Couldn't get appointments" + error })
+
+    }
+
+
+}
--- routes/appointments.js
+++ routes/appointments.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 const getDoctorAppointments  = require("../controllers/getDoctorAppointmentsController");
 const createAppointment = require("../controllers/create_appointment");
+const getPatientAppointments = require("../controllers/get_patient_appointments");
 
 router.post("/getdoctorappointments", getDoctorAppointments.returnDoctorAppointments);
 router.post("/createappointment", createAppointment.createAppointment );
+router.post("/getpatientappointments", getPatientAppointments.getPatientAppointments );
 
 module.exports = router;
