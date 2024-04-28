5e1a0f6e921f1faa33c99b30ad3d8d55095615ae
IannnnnW <ianwamai@gmail.com>
2024-04-28 11:56:48 +0300

    Added history route

--- routes/appointments.js
+++ routes/appointments.js
@@ -6,10 +6,12 @@ const getDoctorAppointments  = require("../controllers/get_doctor_appointments_c
 const createAppointment = require("../controllers/create_appointment");
 const getPatientAppointments = require("../controllers/get_patient_appointments");
 const doctorTakenTimeslots = require("../controllers/get_doctor_taken_timeslots")
+const doctorHistory = require("../controllers/get_doctor_history")
 
 router.post("/getdoctorappointments", getDoctorAppointments.returnDoctorAppointments);
 router.post("/createappointment", createAppointment.createAppointment );
 router.post("/getpatientappointments", getPatientAppointments.getPatientAppointments );
 router.post("/takentimeslots", doctorTakenTimeslots.getDoctorTakenTimeslots);
+router.post("/history", doctorHistory.returnDoctorHistory)
 
 module.exports = router;
