56fb7b02213b323545aad7c96104cbbac09e1580
victor-nsengiyumva <jerrysyre@gmail.com>
2024-06-23 18:38:55 +0300

    some changes to the email templaate

--- controllers/send_mail_controller.js
+++ controllers/send_mail_controller.js
@@ -1,30 +1,42 @@
-const sgMail = require('@sendgrid/mail')
+const sgMail = require("@sendgrid/mail");
 
 exports.sendMail = (req, res, next) => {
+  const userEmail = req.body.userEmail;
+  const callID = req.body.callID;
 
-    const userEmail = req.body.userEmail;
-    const callID = req.body.callID;
+  console.log(userEmail);
+  console.log(callID);
 
-    console.log(userEmail);
-    console.log(callID);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: userEmail, // Change to your recipient
+    from: "jerrysyre@gmail.com", // Change to your verified sender
+    subject: "Mindbridge call ID",
+    html: `
+        <p>Hello from Mindbridge.</p>
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-        to: userEmail, // Change to your recipient
-        from: 'jerrysyre@gmail.com', // Change to your verified sender
-        subject: 'Medbridge call ID',
-        html: `Hello from Medbridge. Call ID is <strong>${callID}</strong>`,
-        // html: '<strong>Your call has started. Please join Now</strong>',
-    }
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log('Email sent');
-            res.send("success")
+         <p>&nbsp;</p>
+         
+         <p>Call ID is <strong>${callID}</strong></p>
 
-        })
-        .catch((error) => {
-            console.error(error)
-            res.send("error")
-        })
-}
+         <br><br>
+
+         <p>Please mark this email as <em>Not Spam</em> incase its in the spam folder</p>
+
+         <p>&nbsp;</p>
+
+         <p>Thank you for using our service.</p>
+         `,
+    // html: '<strong>Your call has started. Please join Now</strong>',
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.send("success");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("error");
+    });
+};
